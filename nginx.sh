2374699e961dea0ce986b337a53c7e35dd0e9933
dottg0n20 <dev@dariocaruso.info>
2022-02-02 19:59:16 +0100

    debug

--- nginx.sh
+++ nginx.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat /proxy.conf | sed s/TESTPSK/${GEO_PSK:-TESTPSK}/g > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
+cat /proxy.conf | sed s/TESTPSK/${GEO_PSK:-TESTPSK}/g > /etc/nginx/conf.d/default.conf && cat /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
