5ff76bc2ba7a44f1a6f7b7bd05ef58153abffd1e
dottg0n20 <dev@dariocaruso.info>
2022-02-02 20:12:00 +0100

    up geo

--- nginx.sh
+++ nginx.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sed s/TESTPSK/AAAA/g /proxy.conf | tee /etc/nginx/nginx.conf && nginx -g "daemon off;"
+sed s/TESTPSK/$GEO_PSK/g /proxy.conf | tee /etc/nginx/nginx.conf && nginx -g "daemon off;"
