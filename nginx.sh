46d8f9f7e21e7c8a468a63e64600ef35bc8f6477
dottg0n20 <dev@dariocaruso.info>
2022-02-02 19:58:01 +0100

    up

--- nginx.sh
+++ nginx.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat /root/proxy.conf | sed s/TESTPSK/${GEO_PSK:-TESTPSK}/g > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
+cat /proxy.conf | sed s/TESTPSK/${GEO_PSK:-TESTPSK}/g > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
